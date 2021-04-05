03d64d629c0b842bd585d08a1186b3b863f983eb
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-04-05 12:18:34 -0500

    updated animal cards

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { BrowserRouter as Router } from "react-router-dom"
-import { Kennel } from "./components/Kennel.js"
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Kennel } from "./components/Kennel.js";
+import "./index.css";
 
 ReactDOM.render(
     <React.StrictMode>
