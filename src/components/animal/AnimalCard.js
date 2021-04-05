60082e4e4dd0c265845372f67535f3e3c968fac4
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-04-05 15:03:44 -0500

    added animals discharge button

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React from 'react';
 import "./Animal.css";
 
-export const AnimalCard = ({ animal }) => {
-    return (
-      <div className="card">
-        <div className="card-content">
-          
-          <h3>Name: <span className="card-petname">
-            {animal.name}
-          </span></h3>
-          <p>Breed: {animal.breed}</p>
-        </div>
+export const AnimalCard = ({ animal, handleDeleteAnimal }) => {
+  return (
+    <div className="card">
+      <div className="card-content">
+        
+        <h3>Name: <span className="card-petname">
+          {animal.name}
+        </span></h3>
+        <p>Breed: {animal.breed}</p>
+        <button type="button" onClick={() => handleDeleteAnimal(animal.id)}>Discharge</button>
       </div>
-    );
-  }
+    </div>
+  );
+}
