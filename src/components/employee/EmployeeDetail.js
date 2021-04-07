7a26b23ca5af67879dc5f8f8a5940cb723ac26d5
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-04-07 10:38:19 -0500

    added all delete buttons to details pages

--- src/components/employee/EmployeeDetail.js
+++ src/components/employee/EmployeeDetail.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { getEmployeeById } from '../../modules/EmployeeManager';
+import { getEmployeeById, deleteEmployee } from '../../modules/EmployeeManager';
 import './EmployeeDetail.css';
 import { useParams, useHistory } from "react-router-dom";
 
 export const EmployeeDetail = () => {
     const [employee, setEmployee] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     const { employeeId } = useParams();
     const history = useHistory();
@@ -17,15 +18,25 @@ export const EmployeeDetail = () => {
                     location: employee.location,
                     animal: employee.animal
                 });
+                setIsLoading(false);
             });
     }, [employeeId]);
 
+    const handleDelete = () => {
+        setIsLoading(true);
+        deleteEmployee(employeeId)
+        .then(() => 
+        history.push("./employees")
+        );
+    };
+
     return (
-        <section className="employee">
+        <section className="employee card">
             <h3 className="employee__name">{employee.name}</h3>
             <div className="employee__location">Location: {employee.location}</div>
             <div className="employee__animal">Cared for: {employee.animal?.name}</div>
-           
+           <button type="button" disabled={isLoading} onClick={handleDelete}>
+               Delete</button> 
         </section>
     );
 }
